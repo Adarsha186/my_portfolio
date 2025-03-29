3fdb6cd191a930297b7d49729a238bfa8740082a
Adarsha Yemparala <67880499+Adarsha186@users.noreply.github.com>
2025-03-29 16:45:13 -0400

    Update Experience.js

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -10,7 +10,14 @@ const Experience = () => {
                 <h1 className='experience-h1'>Experience</h1>
                 <div className='experience-content'>
                     <div className='experience-item'>
-                        <div className='experience-dates'>Oct 2023 – Present</div>
+                        <div className='experience-dates'>June 2024 – Present</div>
+                        <div className='experience-details'>
+                            <h2>Full-Stack Engineer</h2>
+                            <h3>Innovative Tech Explorers LLC</h3>
+                        </div>
+                    </div>
+                    <div className='experience-item'>
+                        <div className='experience-dates'>Oct 2023 – May 2024</div>
                         <div className='experience-details'>
                             <h2>Research Assistant</h2>
                             <h3>California State University - East Bay</h3>
@@ -19,7 +26,7 @@ const Experience = () => {
                     <div className='experience-item'>
                         <div className='experience-dates'>Jan 2022 – Jun 2022</div>
                         <div className='experience-details'>
-                            <h2>Full Stack Developer Intern</h2>
+                            <h2>Software Engineer Intern</h2>
                             <h3>Shiash Info Solutions, Chennai, India</h3>
                         </div>
                     </div>
